0e1124d7342c186aba6863b7619934014a58c984
Frederik <frederik-funke@gmx.de>
2018-05-27 17:06:16 +0000

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
+    passport        = require("passport"),
+    localStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
+
+// requiring routes:
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v13");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// clear DB, add campgrounds, add comments
+// seedDB();
+
+// PASSPORT CONFIGURATION:
+app.use(require("express-session")( {
+  secret: "Rusty",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// make currentUser available in every route, run next code:
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+// use routes files, routes start with "/...":
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+// server:
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Server started");
+});
